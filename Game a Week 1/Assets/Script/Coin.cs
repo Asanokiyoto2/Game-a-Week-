723f0ad47a2dfc6c49e7008ffe2559d64dda69b9
Asanokiyoto <sdt2495gp0001@edu.sca.ac.jp>
2026-06-24 17:10:34 +0900

    no message

--- Game a Week 1/Assets/Script/Coin.cs
+++ Game a Week 1/Assets/Script/Coin.cs
@@ -2,6 +2,24 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+    Vector3 startPos;
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    void Update()
+    {
+        // ‰ñ“]
+        transform.Rotate(0, 180f * Time.deltaTime, 0);
+
+        // ‚Ó‚í‚Ó‚í•‚‚­
+        transform.position =
+            startPos +
+            Vector3.up * Mathf.Sin(Time.time * 2f) * 0.2f;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
